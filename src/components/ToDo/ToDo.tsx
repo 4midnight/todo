b5d1fd82c0a7e70dc85ef68f10f69bb2d7dd1367
4midnight <looncats@gmail.com>
2022-08-11 18:55:37 +0300

    ToDo List Up Date

--- src/components/ToDo/ToDo.tsx
+++ src/components/ToDo/ToDo.tsx
@@ -10,29 +10,32 @@ const ToDo: React.FC = () => {
   const dispatch = useDispatch();
   const [task, setTask] = useState('');
 
-  const formChangeHandler = (event: React.FormEvent<HTMLFormElement>) =>{
+  const formChangeHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    if(task.trim().length == 0){
+    if (task.trim().length === 0) {
       dispatch(errToDoAC("Error. The feild must not empty"));
       return;
     }
 
     dispatch(errToDoAC(null))
-    dispatch(addToDoAC({id: new Date(), name: task}))
+    dispatch(addToDoAC({ id: new Date(), name: task }))
     setTask("")
   }
 
-  const taskInputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) =>{
+  const taskInputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTask(event.target.value)
   }
-  
+
 
   return (
     <div className='todo'>
       <form className="form" onSubmit={formChangeHandler}>
-        {state.err !== null ? <p className='error'>{state.err}</p>: undefined}
-        <input className='todo-input' type="text" placeholder='task' onChange={taskInputChangeHandler} value={task}  />
+        <div className="todo-header">
+          ToDo List
+        </div>
+        {state.err !== null ? <p className='error'>{state.err}</p> : undefined}
+        <input className='todo-input' type="text" placeholder='task' onChange={taskInputChangeHandler} value={task} />
         <ToDoList todo={state.todo} />
       </form>
     </div>
--- src/components/ToDo/ToDoList.tsx
+++ src/components/ToDo/ToDoList.tsx
@@ -2,24 +2,24 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { delToDoAC } from '../../store/reducer/todoReducer';
 
-interface ToDoListProps{
-    todo: any[]
+interface ToDoListProps {
+  todo: any[]
 }
 
-const ToDoList: React.FC<ToDoListProps> = ({todo}) => {
+const ToDoList: React.FC<ToDoListProps> = ({ todo }) => {
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const removeTaskHandler = (event: React.MouseEvent<HTMLLIElement>) =>{
-        console.log(event.currentTarget.id);
-        dispatch(delToDoAC(event.currentTarget.id))
-    }
-    
+  const removeTaskHandler = (event: React.MouseEvent<HTMLLIElement>) => {
+    console.log(event.currentTarget.id);
+    dispatch(delToDoAC(event.currentTarget.id))
+  }
+
 
   return (
     <ul className='todo-list'>
-        {todo.map((item) => (<li key={item.id} id={item.id} className='todo-list-item' onClick={removeTaskHandler}>{item.name}</li>))}
-        
+      {todo.map((item, index) => (<li key={item.id} id={item.id} className='todo-list-item' onClick={removeTaskHandler}>{index + 1}. {item.name}</li>))}
+
     </ul>
   )
 }
